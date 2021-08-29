b634d157ae147f35d5a78e8f73ebb6ddd4a942ee
Mathias Melo <melo.mathias01@gmail.com>
2021-08-29 10:35:52 -0300

    ConectarBD.sh y altaUsuarios.sh con menu mejorado

--- .Datos/conectarBD.sh
+++ .Datos/conectarBD.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 #Parámetros de conexión
-echo Indique los parámetros de conexión:
-read -p "Escriba HOST" host 
-read -p "Escriba USER" user  
-read -sp "Escriba PASSWORD" password
-read -p "Escriba DATABASE " database  
+echo +==============================================+
+echo + Write database parameters:                   +
+echo +==============================================+
+read -p "Escriba HOST: " host 
+echo
+read -p "Escriba USER: " user  
+echo
+read -sp "Escriba PASSWORD: " password
+echo
+read -p "Escriba DATABASE: " database 
+echo +=============================================+ 
 mysql sql_host= $host
 mysql slq_usuario= $user
 mysql sql_password= $password
 mysql sql_database= $database
 
-### Se monta los parámetros de conexión
-sql_args=”-h $sql_host -u $slq_usuario -p$sql_password -D $sql_database -s -e”
-### Mi sentencia Sql para que la muestre
-mysql $sql_args “INSERT INTO users(User_Login,User_Password) VALUES ($userName,$userPassword);”
-mysql $sql_args “INSERT INTO persons(CI,Firt_Name,Second_Name,First_Surname,Second_Surname,Nick_Name) VALUES ($ci,$firstName,$secondName,$firstSurname,$secondSurname,$nickName);”
+
--- Alta/altaUsuarios.sh
+++ Alta/altaUsuarios.sh
@@ -20,37 +20,32 @@
 #}
 
 function userInformation(){
-echo Welcome to Users Register
-echo Write a User Name:
-read userName
-echo Write a Password:
-read userPassword
-echo Confirm Password:
-read confirmUserPassword
-echo Write a CI:
-read ci
-echo Write a First Name:
-read firstName
-echo Write a Second Name:
-read secondName
-echo Write a First Surname:
-read firstSurname
-echo Write a Second Surname:
-read secondSurname
-echo Write a Nick Name 
-read nickName
+echo +==================================================================+
+echo + Welcome to Users Register                                        +
+echo +==================================================================+
+read -p "Write a New User Name: " userName  
+echo                                                                                                           
+read -sp "Write a New Password: " userPassword
+echo 
+read -sp "Confirm Password: " confirmUserPassword
+echo 
+read -p "Write a CI: " ci
+echo 
+read -p "Write a First Name: " firstName
+echo 
+read -p "Write a Second Name: " secondName
+echo 
+read -p "Write a First Surname: " firstSurname
+echo 
+read -p "Write a Second Surname: " secondSurname
+echo 
+read -p "Write a Nick Name: " nickName
+echo +==================================================================+                                                                 
+
 }
-
-### Se monta los parámetros de conexión
-sql_args= -h"localhost" -u "root" -p"maidana10398" -D "sistema_de_chat_institucional" -s -e
-### Mi sentencia Sql para que la muestre
-mysql $sql_args "INSERT INTO users(User_Login,User_Password) VALUES ($userName,$userPassword); "
-mysql $sql_args "INSERT INTO persons(CI,Firt_Name,Second_Name,First_Surname,Second_Surname,Nick_Name) VALUES ($ci,$firstName,$secondName,$firstSurname,$secondSurname,$nickName); "
-
-
-#mysql -u "root" -p"maidana10398." -D "sistema_de_chat_institucional" -e "INSERT INTO users(User_Login,User_Password) VALUES ($userName,$userPassword); "
-#mysql -u "root" -p"maidana10398." -D "sistema_de_chat_institucional" -e "INSERT INTO persons(CI,Firt_Name,Second_Name,First_Surname,Second_Surname,Nick_Name) VALUES ($ci,$firstName,$secondName,$firstSurname,$secondSurname,$nickName); "
-
-#sh ~/source/repos/mathiasmelo10/ShellScriptsMBytes/"Shell Script Operador"/.Datos/bd.sh
 userInformation
+sh .Datos/conectarBD.sh 
+# Sentencia Sql para que la muestre
+mysql -h $sql_host -u $slq_usuario -p$sql_password -D $sql_database -s -e "INSERT INTO users(User_Login,User_Password) VALUES ($userName,$userPassword);"
+mysql -h $sql_host -u $slq_usuario -p$sql_password -D $sql_database -s -e "INSERT INTO persons(CI,Firt_Name,Second_Name,First_Surname,Second_Surname,Nick_Name) VALUES ($ci,$firstName,$secondName,$firstSurname,$secondSurname,$nickName);"
 
